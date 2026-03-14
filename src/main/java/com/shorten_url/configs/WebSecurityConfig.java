9d73a295dfaf4c3f34bf9c941295054685ac29ad
QuocViet <viethoangquoc201@gmail.com>
2026-03-14 09:34:17 +0700

    Remove CSRF

--- src/main/java/com/shorten_url/configs/WebSecurityConfig.java
+++ src/main/java/com/shorten_url/configs/WebSecurityConfig.java
@@ -1,10 +1,6 @@
 package com.shorten_url.configs;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,13 +13,5 @@ public class WebSecurityConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest()
-                .permitAll())
-                .csrf(AbstractHttpConfigurer::disable);
-        return http.build();
     }
 }
