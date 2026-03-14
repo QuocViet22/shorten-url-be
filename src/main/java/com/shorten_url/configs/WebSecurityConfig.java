50d47195fca16e96199e41730d99f8402c0c2c9f
QuocViet <viethoangquoc201@gmail.com>
2026-03-14 22:22:00 +0700

    Disabled CSRF - Update

--- src/main/java/com/shorten_url/configs/WebSecurityConfig.java
+++ src/main/java/com/shorten_url/configs/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,7 +14,7 @@ public class WebSecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("http://127.0.0.1:5500", "https://ziplink.viethq.tech")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
