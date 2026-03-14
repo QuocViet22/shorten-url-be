97cc7ce59ba297113231ded11239ad1cbadca1a4
QuocViet <viethoangquoc201@gmail.com>
2026-03-14 21:47:48 +0700

    Disabled CSRF - Update

--- src/main/java/com/shorten_url/configs/WebSecurityConfig.java
+++ src/main/java/com/shorten_url/configs/WebSecurityConfig.java
@@ -3,12 +3,14 @@ package com.shorten_url.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -22,8 +24,8 @@ public class WebSecurityConfig implements WebMvcConfigurer {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for APIs
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+                // ... other security configurations
+                .csrf(csrf -> csrf.disable()); // Use the lambda DSL to disable CSRF
         return http.build();
     }
 }
